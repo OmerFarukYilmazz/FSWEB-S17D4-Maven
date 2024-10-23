4d4c828187cc90d698bfe06334cc7807837954f5
ÖMER FARUK YILMAZ <omerfaruk2441@gmail.com>
2024-10-23 16:28:53 +0300

    Task done. Tested

--- src/main/java/com/workintech/sqlintro/SqlIntroApplication.java
+++ src/main/java/com/workintech/sqlintro/SqlIntroApplication.java
@@ -3,11 +3,29 @@ package com.workintech.sqlintro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class SqlIntroApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		String jdbcUrl = "jdbc:mysql://localhost:3306/postgres"; // Veritabanı ismini değiştirin
+		String username = "root"; // MySQL kullanıcı adınız
+		String password = "OM5321er?_"; // MySQL şifreniz
+
+		try {
+			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+			if (connection != null) {
+				System.out.println("Bağlantı başarılı!");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(SqlIntroApplication.class, args);
+
 	}
 
 }
